6df3236819c83144fb1ffc515693bc02b9b73222
212571132 <alexsander.dahora@ge.com>
2017-04-28 10:39:09 -0300

    Changed database address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //'database': 'mongodb://smartplug:smartplug@ds119091.mlab.com:19091/smartplug',
-  'database': 'mongodb://localhost:27017/sensors',
+  'database': 'mongodb://smartplug:smartplug@smartplug-shard-00-00-qxtew.mongodb.net:27017,smartplug-shard-00-01-qxtew.mongodb.net:27017,smartplug-shard-00-02-qxtew.mongodb.net:27017/smartplug?ssl=true&replicaSet=SmartPlug-shard-0&authSource=admin',
+  //'database': 'mongodb://localhost:27017/sensors',
   'port': process.env.PORT || 3000,
   'devices': ['microondas']
 };
