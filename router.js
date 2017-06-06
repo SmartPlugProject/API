e2f265ea482eaf3f7c6f80ab3c04d3e8cfba0042
212571132 <alexsander.dahora@ge.com>
2017-06-05 23:35:10 -0300

    Change websocket response

--- router.js
+++ router.js
@@ -162,8 +162,9 @@ module.exports = function(app, wss) {
           message: err.message
         });
       } else {
+        const response = {"id": sensor.id, "value": update};
         wss.clients.forEach((client) => {
-          client.send(JSON.stringify(sensor));
+          client.send(JSON.stringify(response));
         });
         return res.status(200).json({
           success: true,
